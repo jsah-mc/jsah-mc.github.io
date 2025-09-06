832de6c6bf59475ced0c3757837174efc4a3dce5
jsah-mc <sah.joseph@outlook.com>
2025-09-06 17:27:01 -0400

    Changed favicon and name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,13 @@
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Jsah Web", // '%s' will be replaced by specific page titles
+    default: "Jsah Web", // Default title if no specific page title is set
+  },
+  description: "A description of your Next.js application.",
+};
 export default function RootLayout({
   children,
 }: Readonly<{
